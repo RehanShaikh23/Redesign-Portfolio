d115a09f3bbb4e196f796c92cd00a99ca9dc4bfc
Rehan Shaikh <as9565704@gmail.com>
2026-07-28 18:01:46 +0530

    Remove preloader from portfolio app

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,7 @@
-import { useState } from 'react'
-import Preloader from './components/Preloader'
 import DesktopWallpaper from './components/retro/DesktopWallpaper'
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true)
-
-  return (
-    <>
-      {isLoading && (
-        <Preloader
-          brandName="REHAN OS v2.0"
-          onComplete={() => setIsLoading(false)}
-        />
-      )}
-      <DesktopWallpaper />
-    </>
-  )
+  return <DesktopWallpaper />
 }
 
 export default App
